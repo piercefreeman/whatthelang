83764354059403cc1d2887031730f3c70951d603
Pierce Freeman <piercefreeman@gmail.com>
2021-10-07 13:09:06 -0700

    simplify test

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
 from whatthelang import WhatTheLang
 wtl = WhatTheLang()
 
-print(wtl.predict_lang("Mother"))
-print(wtl.pred_prob("Mother"))
-
-print(wtl.predict_lang(["Mother", "Other"]))
-print(wtl.pred_prob(["Mother pero maybe otra lingua", "Other"]))
-
 assert wtl.predict_lang("Mother") == 'en'
 
 assert wtl.predict_lang("தாய்") == 'ta'
